78b6a12a37476a4a2748ecb258689eb3361d480e
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-03-17 07:39:08 -0700

    first commit

---
+++ src/component/chart.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import "chartjs-adapter-date-fns";
+import { enUS } from "date-fns/locale";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, TimeScale);
+
+const BarChart = () => {
+  const [chart, setChart] = useState([]);
+
+  useEffect(() => {
+    const fetchAccountinfo = async () => {
+      const res = await fetch("http://localhost:3000/");
+      const data = await res.json();
+      setChart(data);
+      try {
+        console.log(data);
+      } catch (e) {
+        console.log("Error");
+      }
+    };
+    fetchAccountinfo();
+  }, []);
+
+  var data = {
+    labels: chart?.map((x) => x.equity),
+    datasets: [
+      {
+        label: "${chart?.length}",
+        data: chart?.map((x) => x.balance),
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        type: "time",
+        time: {
+          unit: "second",
+        },
+        adapters: {
+          date: {
+            locale: enUS,
+          },
+        },
+      },
+    },
+    legend: {
+      labels: {
+        fontSize: 26,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Bar data={data} options={options} height={400} />
+    </div>
+  );
+};
+
+export default BarChart;
+
+// -----------------------------------------
+// import React from "react";
+// import {Chart as ChartJS, BarElement} from 'chart.js'
+// import { Line } from "react-chartjs-2";
+
+// function LineChart({ data }) {
+//   return (
+//     <div className="chart-container">
+//       <Line
+//         data={data}
+//         options={{
+//           plugins: {
+//             title: {
+//               display: true,
+//               text: "Account details",
+//             },
+//             legend: {
+//               display: false,
+//             },
+//           },
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default LineChart;
+
+// --------------------------------------
+// import React from "react";
+// import { Line } from "react-chartjs-2";
+
+// function LineChart({ data }) {
+//   return (
+//     <div className="chart-container">
+//       <Line
+//         data={data}
+//         options={{
+//           plugins: {
+//             title: {
+//               display: true,
+//               text: "Account details",
+//             },
+//             legend: {
+//               display: false,
+//             },
+//           },
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default LineChart;
